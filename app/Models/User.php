197ddbad69b773a213bf24e99d259726d463864b
tduong0806 <duongdqt@hblab.vn>
2024-08-27 10:35:12 +0700

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+
+    protected $guard_name = 'web';
+    
     protected $fillable = [
         'name', 'email', 'password', 'role'
     ];
